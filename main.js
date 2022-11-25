2ccfbf4ff0943557a3327bc6f74f3f15538b6c25
Diogo Catalão <diogo.catalao@wtvision.com>
2022-11-25 11:01:39 +0000

    new

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-const product = 'Socks'
+// The create App , takes tghe optionsObject (data) 
+// as an argument, it always takes at leats one empty object
+
+const app = Vue.createApp({
+    data : function(){
+        return{
+            product: 'Socks',
+            description: 'Blue'
+        }
+    }
+})
